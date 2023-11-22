c7a18e68d2360e28247224b567ecd632a513173e
VilelaMarcus <marcusvilela000@gmail.com>
2023-11-22 20:53:46 -0300

    teste

--- src/scenes/dashboard/OS/EditOs.jsx
+++ src/scenes/dashboard/OS/EditOs.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { TextField, Typography, Box, Paper, IconButton } from '@mui/material';
+import { TextField, Typography, Box, Paper, IconButton, Button } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
+import SaveIcon from '@mui/icons-material/Save';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { useReadOsByLaserIdQuery } from './osSlice';
 
@@ -10,8 +11,27 @@ const EditOS = ({ onUpdateOS, onDeleteOS }) => {
   const { equipmentId } = useParams();
   const [editingOSId, setEditingOSId] = useState(null);
   const [editedDescription, setEditedDescription] = useState('');
+  const editRef = useRef(null);
   useReadOsByLaserIdQuery(equipmentId);
   const osList = useSelector((state) => state.os.osList);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (editRef.current && !editRef.current.contains(event.target)) {
+        // Check if the click is on the edit button
+        const isEditButton = event.target.closest('.edit-button');
+        if (!isEditButton) {
+          setEditingOSId(null);
+        }
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
 
   const handleEditClick = (osId, description) => {
     setEditingOSId(osId);
@@ -32,7 +52,7 @@ const EditOS = ({ onUpdateOS, onDeleteOS }) => {
   return (
     <div>
       <Typography variant="h5" gutterBottom>
-        Edit OS for Equipment {equipmentId}
+        Editando OS DO Equipamento {equipmentId}
       </Typography>
       {osList &&
         osList.map((os) => (
@@ -42,25 +62,38 @@ const EditOS = ({ onUpdateOS, onDeleteOS }) => {
             style={{ padding: 16, margin: 16, background: '#748CAB' }}
           >
             {editingOSId === os.id ? (
-              <Box display="flex" flexDirection="column" alignItems="flex-start">
+              <Box ref={editRef} display="flex" flexDirection="column" alignItems="flex-end">
                 <TextField
-                  label="Description"
+                  label="Descrição"
                   value={editedDescription}
                   onChange={(e) => setEditedDescription(e.target.value)}
                   fullWidth
                   margin="dense"
+                  inputProps={{ style: { fontSize: '18px', color: '#fff' } }}
                 />
-                <Box marginTop={1}>
-                  <IconButton color="primary" onClick={handleSaveClick}>
-                    <EditIcon />
-                  </IconButton>
+                <Box mt={2}>
+                  <Button
+                    className="edit-button"
+                    variant="contained"
+                    color="primary"
+                    endIcon={<SaveIcon />}
+                    onClick={handleSaveClick}
+                  >
+                    Salvar
+                  </Button>
                 </Box>
               </Box>
             ) : (
               <Box display="flex" justifyContent="space-between" alignItems="center">
-                <Typography>{os.description}</Typography>
+                <Typography variant="h6" style={{ color: '#fff' }}>
+                  {os.description}
+                </Typography>
                 <Box>
-                  <IconButton color="primary" onClick={() => handleEditClick(os.id, os.description)}>
+                  <IconButton
+                    className="edit-button"
+                    color="primary"
+                    onClick={() => handleEditClick(os.id, os.description)}
+                  >
                     <EditIcon />
                   </IconButton>
                   <IconButton color="secondary" onClick={() => handleDeleteClick(os.id)}>
